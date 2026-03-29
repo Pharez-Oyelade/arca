a3c26c0415a4d4923bd8ce2df648501769f4949d
Pharez <pharezoyelade@gmail.com>
2026-03-29 08:25:16 +0100

    minor fixes

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -24,7 +24,7 @@ const Services = () => {
       <div className="px-4 md:px-8 lg:px-20 bg-[url('/images/apartment1.jpg')] bg-center bg-cover h-[200px] md:h-[280px] lg:h-[320px] rounded-lg relative overflow-hidden group">
         <Link href="/">
           <div className="bg-black/90 absolute h-full w-full top-0 inset-0 z-0" />
-          <div className="hidden md:block bg-[#151717] absolute h-full w-full top-0 inset-0 z-10 translate-y-0 group-hover:-translate-y-full transition-all duration-700 ease-in-out" />
+          <div className="hidden md:block bg-[#151717] absolute h-full w-full top-0 inset-0 z-10 translate-y-0 md:group-hover:-translate-y-full transition-all duration-500 md:duration-700 ease-in-out" />
           <div className="flex flex-col md:flex-row justify-between gap-4 md:gap-8 lg:gap-40 items-center z-50 text-white relative h-full p-4 md:p-6">
             <div className="w-full md:w-[35%] text-sm md:text-base">
               From shortlisting to signing, we guide you through every step — so
@@ -33,14 +33,14 @@ const Services = () => {
 
             <div className="w-full md:w-[40%] text-4xl md:text-6xl lg:text-9xl relative overflow-hidden text-center md:text-left">
               Buy
-              <div className="absolute w-[50%] bottom-1 h-1 bg-white -translate-x-full group-hover:translate-x-0 transition-all duration-700 ease-in-out opacity-0 group-hover:opacity-100" />
+              <div className="absolute w-[50%] bottom-1 h-1 bg-white -translate-x-full md:group-hover:translate-x-0 transition-all duration-500 md:duration-700 ease-in-out opacity-0 md:group-hover:opacity-100" />
             </div>
 
             <div className="flex w-full md:w-[25%] text-2xl md:text-3xl lg:text-5xl justify-center md:justify-end">
               <ArrowRight
                 width={40}
                 height={40}
-                className="md:w-[60px] md:h-[60px] lg:w-[80px] lg:h-[80px] group-hover:translate-x-3 transition-all duration-700 ease-in-out opacity-0 group-hover:opacity-100"
+                className="md:w-[60px] md:h-[60px] lg:w-[80px] lg:h-[80px] md:group-hover:translate-x-3 transition-all duration-500 md:duration-700 ease-in-out opacity-0 md:group-hover:opacity-100"
               />
             </div>
           </div>
@@ -53,7 +53,7 @@ const Services = () => {
       <div className="px-4 md:px-8 lg:px-20 bg-[url('/images/villa.jpg')] bg-center bg-cover h-[200px] md:h-[280px] lg:h-[320px] rounded-lg relative overflow-hidden group">
         <Link href="/">
           <div className="bg-black/90 absolute h-full w-full top-0 inset-0 z-0" />
-          <div className="hidden md:block bg-[#151717] absolute h-full w-full top-0 inset-0 z-10 translate-y-0 group-hover:-translate-y-full transition-all duration-700 ease-in-out" />
+          <div className="hidden md:block bg-[#151717] absolute h-full w-full top-0 inset-0 z-10 translate-y-0 md:group-hover:-translate-y-full transition-all duration-500 md:duration-700 ease-in-out" />
           <div className="flex flex-col md:flex-row justify-between gap-4 md:gap-8 lg:gap-40 items-center z-50 text-white relative h-full p-4 md:p-6">
             <div className="w-full md:w-[35%] text-sm md:text-base">
               We price, present, and position your property to attract the right
@@ -62,14 +62,14 @@ const Services = () => {
 
             <div className="w-full md:w-[40%] text-4xl md:text-6xl lg:text-9xl relative overflow-hidden text-center md:text-left">
               Sell
-              <div className="absolute w-[50%] bottom-1 h-1 bg-white -translate-x-full group-hover:translate-x-0 transition-all duration-700 ease-in-out opacity-0 group-hover:opacity-100" />
+              <div className="absolute w-[50%] bottom-1 h-1 bg-white -translate-x-full md:group-hover:translate-x-0 transition-all duration-500 md:duration-700 ease-in-out opacity-0 md:group-hover:opacity-100" />
             </div>
 
             <div className="w-full md:w-[25%] text-2xl md:text-3xl lg:text-5xl flex justify-center md:justify-end">
               <ArrowRight
                 width={40}
                 height={40}
-                className="md:w-[60px] md:h-[60px] lg:w-[80px] lg:h-[80px] group-hover:translate-x-3 transition-all duration-700 ease-in-out opacity-0 group-hover:opacity-100"
+                className="md:w-[60px] md:h-[60px] lg:w-[80px] lg:h-[80px] md:group-hover:translate-x-3 transition-all duration-500 md:duration-700 ease-in-out opacity-0 md:group-hover:opacity-100"
               />
             </div>
           </div>
@@ -82,7 +82,7 @@ const Services = () => {
       <div className="px-4 md:px-8 lg:px-20 bg-[url('/images/studio.jpg')] bg-center bg-cover h-[200px] md:h-[280px] lg:h-[320px] rounded-lg relative overflow-hidden group">
         <Link href="/">
           <div className="bg-black/90 absolute h-full w-full top-0 inset-0 z-0" />
-          <div className="hidden md:block bg-[#151717] absolute h-full w-full top-0 inset-0 z-10 translate-y-0 group-hover:-translate-y-full transition-all duration-700 ease-in-out" />
+          <div className="hidden md:block bg-[#151717] absolute h-full w-full top-0 inset-0 z-10 translate-y-0 md:group-hover:-translate-y-full transition-all duration-500 md:duration-700 ease-in-out" />
           <div className="flex flex-col md:flex-row justify-between gap-4 md:gap-8 lg:gap-40 items-center z-50 text-white relative h-full p-4 md:p-6">
             <div className="w-full md:w-[35%] text-sm md:text-base">
               Whether you're a tenant or a landlord, we handle the search,
@@ -91,14 +91,14 @@ const Services = () => {
 
             <div className="w-full md:w-[40%] text-4xl md:text-6xl lg:text-9xl relative overflow-hidden text-center md:text-left">
               Rent
-              <div className="absolute w-[50%] bottom-1 h-1 bg-white -translate-x-full group-hover:translate-x-0 transition-all duration-700 ease-in-out opacity-0 group-hover:opacity-100" />
+              <div className="absolute w-[50%] bottom-1 h-1 bg-white -translate-x-full md:group-hover:translate-x-0 transition-all duration-500 md:duration-700 ease-in-out opacity-0 md:group-hover:opacity-100" />
             </div>
 
             <div className="w-full md:w-[25%] text-2xl md:text-3xl lg:text-5xl flex justify-center md:justify-end">
               <ArrowRight
                 width={40}
                 height={40}
-                className="md:w-[60px] md:h-[60px] lg:w-[80px] lg:h-[80px] group-hover:translate-x-3 transition-all duration-700 ease-in-out opacity-0 group-hover:opacity-100"
+                className="md:w-[60px] md:h-[60px] lg:w-[80px] lg:h-[80px] md:group-hover:translate-x-3 transition-all duration-500 md:duration-700 ease-in-out opacity-0 md:group-hover:opacity-100"
               />
             </div>
           </div>
