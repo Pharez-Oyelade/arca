1c2f5e666aa1e81fbb85626558c8dad400927ea1
Pharez <pharezoyelade@gmail.com>
2026-03-29 23:11:57 +0100

    entered basic details

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -3,6 +3,8 @@
 import { useProductContext } from "@/app/context/ProductContext";
 import { useParams } from "next/navigation";
 import React from "react";
+import Image from "next/image";
+import { Map } from "lucide-react";
 
 const page = () => {
   const params = useParams();
@@ -11,7 +13,41 @@ const page = () => {
   const { products } = useProductContext();
   const idNum = Number(id);
   const product = products.find((p) => p.id === idNum);
-  return <div>page</div>;
+
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+  return (
+    <section>
+      <div className="w-full h-[600px] overflow-hidden">
+        <Image
+          src={product.image}
+          alt={product.name}
+          width={700}
+          height={700}
+          className="w-full h-full"
+        ></Image>
+      </div>
+
+      <div className="pt-5 flex gap-2">
+        <div className="border-2 py-2 px-5 capitalize rounded-full">
+          {product.type}
+        </div>
+        <div className="border-2 py-2 px-5 capitalize border-green-500 rounded-full">
+          {product.saleType}
+        </div>
+      </div>
+
+      <div className="pt-2">
+        <h1 className="text-3xl font-bold">{product.name}</h1>
+
+        <div className="flex gap-2">
+          <Map className="text-gray-600" />
+          <p>{product.location}</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default page;
