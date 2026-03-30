d4665a3d58b34c5cd5ce46775ee37e98d2088fb8
Pharez <pharezoyelade@gmail.com>
2026-03-30 11:57:30 +0100

    sale filter setup

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,50 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Filters from "./components/Filters";
 import ProductGrid from "./components/ProductGrid";
+import { properties } from "../assets/properties";
 
 const page = () => {
+  const [saleType, setSaleType] = useState("");
+  const [saleDropdown, setSaleDropdown] = useState(false);
+
+  const [products, setProducts] = useState(properties);
+
+  // Apply Filters
+  const applyFilters = () => {
+    let filteredProducts = properties;
+
+    if (saleType === "All") {
+      filteredProducts = properties;
+    }
+
+    if (saleType === "For Sale") {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.saleType === "for sale",
+      );
+    } else if (saleType === "For Rent") {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.saleType === "for rent",
+      );
+    }
+    setProducts(filteredProducts);
+  };
+
+  useEffect(() => {
+    applyFilters();
+  }, [saleType]);
   return (
     <div>
-      <Filters />
+      <Filters
+        products={properties}
+        saleType={saleType}
+        setSaleType={setSaleType}
+        saleDropdown={saleDropdown}
+        setSaleDropdown={setSaleDropdown}
+      />
       <div>
-        <ProductGrid />
+        <ProductGrid products={products} />
       </div>
     </div>
   );
